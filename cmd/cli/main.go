5363b916f2aee85c30caa275419f2605760e9fb0
Brennon Loveless <brennon.loveless@gmail.com>
2020-05-30 21:42:07 -0400

    Finished the board to text conversion

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "twittership"
+import (
+	"fmt"
+	"log"
+	"twittership"
+)
 
 func main() {
-	twittership.PrintGameTextFromGame()
+	g := twittership.NewGame()
+	err := g.LoadPlayerShips("A1H;B8V;E3H;G3V;H8H")
+	if err != nil {
+		log.Fatalf("Unable to load player ships: %v", err)
+	}
+
+	err = g.LoadEnemyShips("A1H;B8V;E3H;G3V;H8H")
+	if err != nil {
+		log.Fatalf("Unable to load enemy ships: %v", err)
+	}
+
+	err = g.LoadPlayerVolleys("A1;B1;C8")
+	if err != nil {
+		log.Fatalf("Unable to load player volleys: %s", err)
+	}
+
+	err = g.LoadEnemyVolleys("A1;B1;C8")
+	if err != nil {
+		log.Fatalf("Unable to load enemy volleys: %s", err)
+	}
+
+	fmt.Println(twittership.GetGameTextFromGame(g))
 }
--- text.go
+++ text.go
@@ -1,63 +1,58 @@
 package twittership
 
-import "fmt"
-
-func blue(message string) string {
-	return fmt.Sprintf("\x1b[34m%s\x1b[0m", message)
-}
+import (
+	"fmt"
+)
 
 func blueBg(message string) string {
 	return fmt.Sprintf("\x1b[44m%s\x1b[0m", message)
-}
-
-func red(message string) string {
-	return fmt.Sprintf("\x1b[31m%s\x1b[0m", message)
 }
 
 func redBg(message string) string {
 	return fmt.Sprintf("\x1b[41m%s\x1b[0m", message)
 }
 
-func grey(message string) string {
-	return fmt.Sprintf("\x1b[90m%s\x1b[0m", message)
-}
-
-func greyBg(message string) string {
-	return fmt.Sprintf("\x1b[99m%s\x1b[0m", message)
-}
-
-func printTile(tile boardTile) {
-
-}
-
-func PrintGameTextFromGame() {
-	fmt.Println("|-----------------------------------------------------------------|")
-	fmt.Println("|         PLAYER BOARD          | |          ENEMY BOARD          |")
-	fmt.Println("|-----------------------------------------------------------------|")
-	fmt.Println("|-| 1| 2| 3| 4| 5| 6| 7| 8| 9|10| |-| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|")
-
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-
-		}
-
-		for j := 0; j < 10; j++ {
-
-		}
+func getTileString(tile boardTile, isEnemyBoard bool) string {
+	// Ship with no volley
+	if tile.shipIndex != -1 && tile.volleyIndex == -1 && !isEnemyBoard {
+		return blueBg(" ")
 	}
 
-	fmt.Println("|A|  |  |  |  |  |  |  |  |  |  | |A|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|B|  |  |  |  |  |  |  |  |  |  | |B|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|C|  |  |  |  |  |  |  |  |  |  | |C|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|D|  |  |  |  |  |  |  |  |  |  | |D|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|E|  |  |  |  |  |  |  |  |  |  | |E|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|F|  |  |  |  |  |  |  |  |  |  | |F|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|G|  |  |  |  |  |  |  |  |  |  | |G|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|H|  |  |  |  |  |  |  |  |  |  | |H|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|I|  |  |  |  |  |  |  |  |  |  | |I|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|J|  |  |  |  |  |  |  |  |  |  | |J|  |  |  |  |  |  |  |  |  |  |")
-	fmt.Println("|-----------------------------------------------------------------|")
+	// Hit volley
+	if tile.shipIndex != -1 && tile.volleyIndex != -1 {
+		return redBg("X")
+	}
 
-	fmt.Printf("|%s|\n", blueBg(grey("X")))
-	fmt.Printf("|%s|\n", redBg(grey("X")))
+	// Missed volley
+	if tile.shipIndex == -1 && tile.volleyIndex != -1 {
+		return "X"
+	}
+
+	// Tile with no ship and no volley
+	return " "
+}
+
+func GetGameTextFromGame(g Game) string {
+	output := "|---------------------------------------------|\n"
+	output += "|     PLAYER BOARD    | |     ENEMY BOARD     |\n"
+	output += "|---------------------------------------------|\n"
+	output += "|-|1|2|3|4|5|6|7|8|9|\u042E| |-|1|2|3|4|5|6|7|8|9|\u042E|\n"
+
+	for y := 0; y < 10; y++ {
+		output += fmt.Sprintf("|%s", string('A'+y))
+
+		for x := 0; x < 10; x++ {
+			output += fmt.Sprintf("|%s", getTileString(g.playerBoard[y][x], false))
+		}
+
+		output += fmt.Sprintf("| |%s", string('A'+y))
+
+		for x := 0; x < 10; x++ {
+			output += fmt.Sprintf("|%s", getTileString(g.enemyBoard[y][x], true))
+		}
+
+		output += "|\n"
+	}
+
+	return output
 }
